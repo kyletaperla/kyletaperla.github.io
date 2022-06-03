2a242c03f19012437fbc09ee2e3c0b2ee8eaa5fb
Kyle Taperla <kyle.taperla@gmail.com>
2022-06-02 22:02:00 -0500

    added ethereum basic ui

---
+++ src/components/crypto-price.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ethereum from "../images/eth.png";
+import "./crypto-price.css";
+import { useStore as useCryptoStore } from './crypto-price-store';
+
+
+export { CryptoPrice }
+
+const CryptoPrice = () => {
+    const { cryptoPrice, getCryptoPrice } = useCryptoStore(state => state);
+    const minute_ms = 60000;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getCryptoPrice();
+          }, minute_ms);
+        
+          return () => clearInterval(interval);
+    }, [])
+    
+    return (
+        <div className='card'>
+            <div className='row'>
+                <div className='col-12 col-md-6 col-lg-12'>
+                    <img className={ethereum} src={ethereum}></img>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-12 col-md-6 col-lg-12'>
+                    <span>Ethereum</span>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-12 col-md-6 col-lg-12'>
+                    {cryptoPrice ? <span className='subtitle'>{`$${cryptoPrice}`}</span> : null}
+                </div>
+            </div>
+        </div>
+    )
+}
+
