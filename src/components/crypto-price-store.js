35ac59eb8314690f8ab2b80356ab7d0733ff723a
Kyle Taperla <kyle.taperla@gmail.com>
2022-06-03 01:51:19 -0500

    created multiple cards for different tokens

--- src/components/crypto-price-store.js
+++ src/components/crypto-price-store.js
@@ -3,20 +3,34 @@ import axios from 'axios';
 
 export { useStore }
 
-const useStore = create(set => ({
-  cryptoPrice: null,
-  setCryptoPrice: (price) => {
-      set({cryptoPrice: price});
-  },
-  getCryptoPrice: () => {
-    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
-    .then((response) => {
-      set({
-          cryptoPrice: response.data.ethereum.usd
+const useStore = create((set, get) => ({
+    cryptocurrencies: [
+        {key: 0, value:'ethereum', cryptoPrice: null},
+        {key: 1, value:'bitcoin', cryptoPrice: null},
+        {key: 2, value:'dogecoin', cryptoPrice: null},
+        {key: 3, value:'shibainu', cryptoPrice: null}
+    ],
+    getCryptoPrice: (crypto) => {
+      axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${crypto}&vs_currencies=usd`)
+      .then((response) => {
+        set(state => {
+            let cryptocurrencies = state.cryptocurrencies;
+            let result = Object.values(response.data)[0];
+
+            (cryptocurrencies || []).map(c => {
+                if (c.value === crypto) {
+                    console.log(result.usd);
+                    c.cryptoPrice = result.usd;
+                }
+            })
+
+            return {
+                cryptocurrencies: cryptocurrencies
+            }
+        })
       })
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
+      .catch((error) => {
+        console.log(error);
+      })
+    }
 }))
--- src/components/crypto-price.jsx
+++ src/components/crypto-price.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ethereum from "../images/eth.png";
 import "./crypto-price.css";
 import { useStore as useCryptoStore } from './crypto-price-store';
 
+import ethereum from '../images/ethereum.png';
+import bitcoin from '../images/bitcoin.png';
 
 export { CryptoPrice }
 
 const CryptoPrice = () => {
-    const { cryptoPrice, getCryptoPrice } = useCryptoStore(state => state);
+    const cryptocurrencies = useCryptoStore(state => state.cryptocurrencies);
+    return (
+        <>{(cryptocurrencies || []).map(c => <CryptoPriceCard key={c.key} crypto={c.value} price={c.cryptoPrice} />)}</>
+    )
+}
+
+const CryptoPriceCard = ({ crypto, price }) => {
+    const { getCryptoPrice } = useCryptoStore(state => state);
     const minute_ms = 60000;
 
     useEffect(() => {
-        getCryptoPrice();
+        getCryptoPrice(crypto);
 
         const interval = setInterval(() => {
-            getCryptoPrice();
+            getCryptoPrice(crypto);
         }, minute_ms);
         
           return () => clearInterval(interval);
@@ -25,17 +33,17 @@ const CryptoPrice = () => {
         <div className='card'>
             <div className='row'>
                 <div className='col-12 col-md-6 col-lg-12'>
-                    <img className={ethereum} src={ethereum}></img>
+                    <img src={require(`../images/${crypto}.png`)}></img>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-12 col-md-6 col-lg-12'> 
+                    <span>{crypto}</span>
                 </div>
             </div>
             <div className='row'>
                 <div className='col-12 col-md-6 col-lg-12'>
-                    <span>Ethereum</span>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col-12 col-md-6 col-lg-12'>
-                    {cryptoPrice ? <span className='subtitle'>{`$${cryptoPrice}`}</span> : null}
+                    {price ? <span className='subtitle'>{`$${price}`}</span>: null}
                 </div>
             </div>
         </div>
